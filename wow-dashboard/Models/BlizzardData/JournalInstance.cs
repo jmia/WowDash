ae8db6b0f8e732ca5207a544090778094b678f50
Jen Armstrong <jarmstrong@reliqhealth.com>
2020-07-29 21:41:16 -0400

    Add journal instance controller and models

---
+++ wow-dashboard/Models/BlizzardData/JournalInstance.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.Json.Serialization;
+
+namespace wow_dashboard.Models.BlizzardData
+{
+
+    public class JournalInstance
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+        [JsonPropertyName("encounters")]
+        public Encounter[] Encounters { get; set; }
+        [JsonPropertyName("modes")]
+        public ModeWrapper[] Modes { get; set; }
+
+        public class Encounter
+        {
+            [JsonPropertyName("name")]
+            public string Name { get; set; }
+            [JsonPropertyName("id")]
+            public int Id { get; set; }
+        }
+
+        public class ModeWrapper
+        {
+            [JsonPropertyName("mode")]
+            public Mode Mode { get; set; }
+        }
+
+        public class Mode
+        {
+            [JsonPropertyName("name")]
+            public string Name { get; set; }
+        }
+    }
+
+}
---
+++ wow-dashboard/Models/BlizzardData/JournalInstanceIndex.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace wow_dashboard.Models.BlizzardData
+{
+
+    public class JournalInstanceIndex
+    {
+        [JsonPropertyName("instances")]
+        public JournalInstance[] Instances { get; set; }
+    }
+
+}
