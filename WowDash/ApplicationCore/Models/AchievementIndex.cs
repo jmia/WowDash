b54cae24904b13e358d86ff8c27275a5890f962a
jmia <jen.armstrong89@gmail.com>
2020-08-17 18:29:43 -0400

    Add achievement controller and models

---
+++ WowDash/ApplicationCore/Models/AchievementIndex.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WowDash.ApplicationCore.Models
+{
+    public class AchievementIndex
+    {
+        [JsonPropertyName("achievements")]
+        public Achievement[] Achievements { get; set; }
+    }
+}
