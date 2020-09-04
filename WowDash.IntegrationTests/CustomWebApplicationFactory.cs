d93392eef1ef88d81e73a4adcec5439ce946bf45
jmia <jen.armstrong89@gmail.com>
2020-09-04 15:24:14 -0400

    Remove commented code

--- WowDash.IntegrationTests/CustomWebApplicationFactory.cs
+++ WowDash.IntegrationTests/CustomWebApplicationFactory.cs
@@ -39,45 +39,6 @@ namespace WowDash.IntegrationTests
                         .GetRequiredService<ILogger<CustomWebApplicationFactory>>();
 
                     db.Database.Migrate();
-
-                    try
-                    {
-                        // These are wiped before the first test run, so they need a better home
-                        //var defaultUser = new Player { DisplayName = "Jen", DefaultRealm = "area-52" };
-
-                        //db.Players.Add(defaultUser);
-
-                        //var scully = new Character
-                        //{
-                        //    Name = "Scully",
-                        //    Gender = CharacterGender.Female,
-                        //    Realm = "area-52",
-                        //    Class = "Hunter",
-                        //    Race = "Blood Elf",
-                        //    Level = 120,
-                        //    PlayerId = defaultUser.Id
-                        //};
-
-                        //var chakwas = new Character
-                        //{
-                        //    Name = "Chakwas",
-                        //    Gender = CharacterGender.Female,
-                        //    Realm = "area-52",
-                        //    Class = "Druid",
-                        //    Race = "Highmountain Tauren",
-                        //    Level = 120,
-                        //    PlayerId = defaultUser.Id
-                        //};
-
-                        //db.Characters.AddRange(scully, chakwas);
-
-                        //db.SaveChanges();
-                    }
-                    catch (Exception ex)
-                    {
-                        logger.LogError(ex, "An error occurred seeding the " +
-                            "database with test messages. Error: {Message}", ex.Message);
-                    }
                 }
             });
         }
