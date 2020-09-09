b63cc81c144db50695b321cc7c12b39dba4f6f1e
jmia <jen.armstrong89@gmail.com>
2020-09-08 22:21:39 -0400

    Add DeleteTask and tests

---
+++ WowDash.Tests/Tasks/DeleteTaskTests.cs
@@ -0,0 +1,93 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using WowDash.ApplicationCore.DTO;
+using WowDash.ApplicationCore.Entities;
+using WowDash.UnitTests.Common;
+using WowDash.WebUI.Controllers;
+using static WowDash.ApplicationCore.Common.Enums;
+
+namespace WowDash.UnitTests.Tasks
+{
+    [TestFixture]
+    public class DeleteTaskTests : UnitTestBase
+    {
+        private Player _defaultPlayer;
+        private TasksController _controller;
+
+        [SetUp]
+        public void Setup()
+        {
+            Assume.That(Context.Players.Any(), "The testing database needs at least one user.");
+            _defaultPlayer = Context.Players.First();
+            _controller = new TasksController(Context);
+        }
+
+        [Test]
+        public void GivenAValidTask_DeletesTaskFromDatabase()
+        {
+            // Arrange
+            var task = new Task(_defaultPlayer.Id, TaskType.General);
+
+            Context.Tasks.Add(task);
+            Context.SaveChanges();
+
+            var dto = new DeleteTaskRequest(task.Id);
+
+            // Act
+            var result = _controller.DeleteTask(dto);
+
+            var removedTask = Context.Tasks.Find(result.Value);
+
+            // Assert
+            removedTask.Should().BeNull();
+        }
+
+        [Test]
+        public void GivenAValidTaskWithTaskCharacters_DeletesAllAssociatedTaskCharactersFromDatabase()
+        {
+            // Arrange
+            var task = new Task(_defaultPlayer.Id, TaskType.General);
+
+            var scully = Context.Characters.Where(c => c.Name.Equals("Scully")).FirstOrDefault();
+            var chakwas = Context.Characters.Where(c => c.Name.Equals("Chakwas")).FirstOrDefault();
+
+            Context.Tasks.Add(task);
+            Context.SaveChanges();
+
+            var taskCharacters = new List<TaskCharacter>
+            {
+                new TaskCharacter(scully.Id, task.Id),
+                new TaskCharacter(chakwas.Id, task.Id),
+            };
+
+            Context.TaskCharacters.AddRange(taskCharacters);
+            Context.SaveChanges();
+
+            var dto = new DeleteTaskRequest(task.Id);
+
+            // Act
+            var result = _controller.DeleteTask(dto);
+
+            var removedTaskCharacters = Context.TaskCharacters.Where(tc => tc.TaskId == task.Id);
+
+            // Assert
+            removedTaskCharacters.Should().BeEmpty();
+        }
+
+        [Test]
+        public void GivenAnInvalidTask_ReturnsNotFound()
+        {
+            // Arrange
+            var dto = new DeleteTaskRequest(TestConstants.AllOnesGuid);
+
+            // Act
+            var result = _controller.DeleteTask(dto);
+
+            // Assert
+            Assert.IsInstanceOf<NotFoundResult>(result.Result);
+        }
+    }
+}
---
+++ WowDash/ApplicationCore/DTO/DeleteTaskRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WowDash.ApplicationCore.DTO
+{
+    public class DeleteTaskRequest
+    {
+        public Guid TaskId { get; }
+
+        public DeleteTaskRequest(Guid taskId)
+        {
+            TaskId = taskId;
+        }
+    }
+}
