120010971da272a09e85ba87373610713637eceb
jmia <jen.armstrong89@gmail.com>
2020-09-11 10:59:30 -0400

    WIP add tests

---
+++ WowDash.Tests/Tasks/SetTaskCollectibleTypeAndSourceTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System.Linq;
+using WowDash.ApplicationCore.Entities;
+using WowDash.UnitTests.Common;
+using WowDash.WebUI.Controllers;
+
+namespace WowDash.UnitTests.Tasks
+{
+    [TestFixture]
+    public class SetTaskCollectibleTypeAndSourceTests : UnitTestBase
+    {
+        private Player _defaultPlayer;
+        private TasksController _controller;
+
+        [SetUp]
+        public void Setup()
+        {
+            Assume.That(Context.Players.Any(), "The testing database needs at least one user.");
+            _defaultPlayer = Context.Players.First();
+            _controller = new TasksController(Context);
+        }
+
+
+    }
+}
