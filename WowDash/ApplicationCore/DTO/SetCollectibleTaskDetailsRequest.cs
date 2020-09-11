f8cf163121f2cc835de9a791a9ae4f357c6cfb9c
jmia <jen.armstrong89@gmail.com>
2020-09-11 19:38:14 -0400

    Add missing required annotations

--- WowDash/ApplicationCore/DTO/SetCollectibleTaskDetailsRequest.cs
+++ WowDash/ApplicationCore/DTO/SetCollectibleTaskDetailsRequest.cs
@@ -1,13 +1,17 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using static WowDash.ApplicationCore.Common.Enums;
 
 namespace WowDash.ApplicationCore.DTO
 {
     public class SetCollectibleTaskDetailsRequest
     {
+        [Required]
         public Guid TaskId { get; }
         public string Description { get; }
+        [Required]
         public RefreshFrequency RefreshFrequency { get; }
+        [Required]
         public Priority Priority { get; }
 
         public SetCollectibleTaskDetailsRequest(Guid taskId, string description, RefreshFrequency refreshFrequency,
--- WowDash/ApplicationCore/DTO/SetTaskCollectibleTypeAndSourceRequest.cs
+++ WowDash/ApplicationCore/DTO/SetTaskCollectibleTypeAndSourceRequest.cs
@@ -1,12 +1,16 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using static WowDash.ApplicationCore.Common.Enums;
 
 namespace WowDash.ApplicationCore.DTO
 {
     public class SetTaskCollectibleTypeAndSourceRequest
     {
+        [Required]
         public Guid TaskId { get; set; }
+        [Required]
         public CollectibleType CollectibleType { get; set; }
+        [Required]
         public Source Source { get; set; }
 
         public SetTaskCollectibleTypeAndSourceRequest(Guid taskId, CollectibleType collectibleType, Source source)
