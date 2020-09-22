0aca3a50e68cff67ebac8555f9d9921ce0a9dcc5
Jen Armstrong <jen.armstrong89@gmail.com>
2020-09-22 11:44:28 -0400

    Clean up item controller, add integration tests (#107)

--- WowDash/WebUI/Controllers/ItemsController.cs
+++ WowDash/WebUI/Controllers/ItemsController.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Text.Json;
@@ -18,8 +17,17 @@ namespace WowDash.WebUI.Controllers
         public ItemsController(IHttpClientFactory clientFactory, IConfiguration configuration)
             : base(clientFactory, configuration) { }
 
+        /// <summary>
+        /// Get an item by its game ID.
+        /// </summary>
+        /// <param name="id">The game ID of the item.</param>
+        /// <response code="200">Returns the resource.</response>
+        /// <response code="400">If the request is null or missing required fields.</response>
+        /// <response code="401">If the client failed to authorize an access token.</response>
+        /// <response code="404">If the resource was not found.</response>
         [HttpGet("{id}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<Item>> GetItem(int id)
@@ -34,36 +42,36 @@ namespace WowDash.WebUI.Controllers
 
             request.Headers.TryAddWithoutValidation("Authorization", $"Bearer {access_token}");
 
-            try
+            var response = await client.SendAsync(request);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
+                return NotFound();
+
+            if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+                return Unauthorized();
+
+            if (response.IsSuccessStatusCode)
             {
-                var response = await client.SendAsync(request);
+                var content = response.Content.ReadAsStreamAsync();
+                var item = await JsonSerializer.DeserializeAsync<Item>(await content);
 
-                if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
-                    return NotFound();
-
-                if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
-                    return Unauthorized();
-
-                if (response.IsSuccessStatusCode)
-                {
-                    var content = response.Content.ReadAsStreamAsync();
-                    var item = await JsonSerializer.DeserializeAsync<Item>(await content);
-
-                    return Ok(item);
-                }
-                else
-                {
-                    throw new Exception("The response was status code " + response.StatusCode);
-                }
+                return Ok(item);
             }
-            catch (Exception ex)
-            {
-                throw new Exception("Unhandled status code from 'GetItem': " + ex.Message);
-            }
+
+            return StatusCode((int)response.StatusCode);
         }
 
+        /// <summary>
+        /// Search for an item by its name (partials accepted, case insensitive).
+        /// </summary>
+        /// <param name="name">The search term.</param>
+        /// <response code="200">Returns a list of matched resources.</response>
+        /// <response code="400">If the request is null or missing required fields.</response>
+        /// <response code="401">If the client failed to authorize an access token.</response>
+        /// <response code="404">If the resource was not found.</response>
         [HttpGet("search/{name}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<IEnumerable<Item>>> SearchItemsByItemName(string name)
@@ -79,33 +87,24 @@ namespace WowDash.WebUI.Controllers
 
             request.Headers.TryAddWithoutValidation("Authorization", $"Bearer {access_token}");
 
-            try
+            var response = await client.SendAsync(request);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
+                return NotFound();
+
+            if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+                return Unauthorized();
+
+            if (response.IsSuccessStatusCode)
             {
-                var response = await client.SendAsync(request);
+                var content = response.Content.ReadAsStreamAsync();
+                var searchResult = await JsonSerializer.DeserializeAsync<BlizzardSearchResult>(await content);
 
-                if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
-                    return NotFound();
-
-                if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
-                    return Unauthorized();
-
-                if (response.IsSuccessStatusCode)
-                {
-                    var content = response.Content.ReadAsStreamAsync();
-                    var searchResult = await JsonSerializer.DeserializeAsync<BlizzardSearchResult>(await content);
-
-                    return Ok(searchResult.Results.Select(r => new Item { Id = r.Data.Id, Name = r.Data.Name.en_US })
-                                               .ToList());
-                }
-                else
-                {
-                    throw new Exception("The response was status code " + response.StatusCode);
-                }
+                return Ok(searchResult.Results.Select(r => new Item { Id = r.Data.Id, Name = r.Data.Name.en_US })
+                                            .ToList());
             }
-            catch (Exception ex)
-            {
-                throw new Exception("Unhandled status code from 'SearchItemsByItemName': " + ex.Message);
-            }
+
+            return StatusCode((int)response.StatusCode);
         }
     }
 }
