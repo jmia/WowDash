09dd344a3291220b610483f8854d1bfb3e203c70
jmia <jen.armstrong89@gmail.com>
2020-09-24 11:56:18 -0400

    Cleanup QuestArea, fix bugs, add integration tests

--- WowDash.IntegrationTests/Blizzard/QuestAreaQueryTests.cs
+++ WowDash.IntegrationTests/Blizzard/QuestAreaQueryTests.cs
@@ -10,16 +10,16 @@ using WowDash.ApplicationCore.Models;
 namespace WowDash.IntegrationTests.Blizzard
 {
     [TestFixture]
-    public class JournalInstanceQueryTests : IntegrationTestBase
+    public class QuestAreaQueryTests : IntegrationTestBase
     {
         [Test]
-        public async Task GetJournalInstance_ReturnsJournalInstance()
+        public async Task GetQuestArea_ReturnsQuestArea()
         {
-            var expectedGameId = 751;
-            var expectedName = "Black Temple";
+            var expectedGameId = 15;
+            var expectedName = "Dustwallow Marsh";
 
             // Act
-            var httpResponse = await Client.GetAsync($"/api/dungeons/{expectedGameId}");
+            var httpResponse = await Client.GetAsync($"/api/zones/{expectedGameId}");
 
             httpResponse.EnsureSuccessStatusCode();
 
@@ -30,7 +30,7 @@ namespace WowDash.IntegrationTests.Blizzard
                 PropertyNameCaseInsensitive = true,
             };
 
-            var result = await JsonSerializer.DeserializeAsync<JournalInstance>(response, options);
+            var result = await JsonSerializer.DeserializeAsync<QuestArea>(response, options);
 
             // Assert
             result.Id.Should().Be(expectedGameId);
@@ -38,12 +38,12 @@ namespace WowDash.IntegrationTests.Blizzard
         }
 
         [Test]
-        public async Task SearchJournalInstancesByName_ReturnsList()
+        public async Task SearchQuestAreasByName_ReturnsList()
         {
-            var searchTerm = "karazhan";
+            var searchTerm = "zul";
 
             // Act
-            var httpResponse = await Client.GetAsync($"/api/dungeons/search/{searchTerm}");
+            var httpResponse = await Client.GetAsync($"/api/zones/search/{searchTerm}");
 
             httpResponse.EnsureSuccessStatusCode();
 
@@ -54,7 +54,7 @@ namespace WowDash.IntegrationTests.Blizzard
                 PropertyNameCaseInsensitive = true,
             };
 
-            var result = await JsonSerializer.DeserializeAsync<IEnumerable<JournalInstance>>(response, options);
+            var result = await JsonSerializer.DeserializeAsync<IEnumerable<QuestArea>>(response, options);
 
             // Assert
             result.Should().NotBeEmpty();
--- WowDash/WebUI/Controllers/QuestAreasController.cs
+++ WowDash/WebUI/Controllers/QuestAreasController.cs
@@ -18,53 +18,17 @@ namespace WowDash.WebUI.Controllers
         public QuestAreasController(IHttpClientFactory clientFactory, IConfiguration configuration)
             : base(clientFactory, configuration) { }
 
-        [HttpGet]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<ActionResult<IEnumerable<QuestArea>>> GetQuestAreas()
-        {
-            var client = _clientFactory.CreateClient();
-
-            var access_token = await GetAccessTokenAsync();
-
-            using var request = new HttpRequestMessage(new HttpMethod("GET"),
-                "https://us.api.blizzard.com/data/wow/quest/area/index?" +
-                "namespace=static-us&locale=en_US");
-
-            request.Headers.TryAddWithoutValidation("Authorization", $"Bearer {access_token}");
-
-            try
-            {
-                var response = await client.SendAsync(request);
-
-                if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
-                    return NotFound();
-
-                if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
-                    return Unauthorized();
-
-                if (response.IsSuccessStatusCode)
-                {
-                    var content = response.Content.ReadAsStreamAsync();
-                    var index = await JsonSerializer.DeserializeAsync<QuestAreaIndex>(await content);
-
-                    return Ok(index.Areas.Select(a => new QuestArea { Id = a.Id, Area = a.Name }).ToList());
-                }
-                else
-                {
-                    throw new Exception("The response was status code " + response.StatusCode);
-                }
-            }
-            catch (Exception ex)
-            {
-                throw new Exception("Unhandled status code from 'GetQuestAreas': " + ex.Message);
-            }
-
-        }
-
+        /// <summary>
+        /// Get a zone by its game ID.
+        /// </summary>
+        /// <param name="id">The game ID of the zone.</param>
+        /// <response code="200">Returns the resource.</response>
+        /// <response code="400">If the request is null or missing required fields.</response>
+        /// <response code="401">If the client failed to authorize an access token.</response>
+        /// <response code="404">If the resource was not found.</response>
         [HttpGet("{id}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<QuestArea>> GetQuestArea(int id)
@@ -79,35 +43,33 @@ namespace WowDash.WebUI.Controllers
 
             request.Headers.TryAddWithoutValidation("Authorization", $"Bearer {access_token}");
 
-            try
+            var response = await client.SendAsync(request);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
+                return NotFound();
+
+            if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+                return Unauthorized();
+
+            if (response.IsSuccessStatusCode)
             {
-                var response = await client.SendAsync(request);
+                var content = response.Content.ReadAsStreamAsync();
+                var questArea = await JsonSerializer.DeserializeAsync<QuestArea>(await content);
 
-                if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
-                    return NotFound();
-
-                if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
-                    return Unauthorized();
-
-                if (response.IsSuccessStatusCode)
-                {
-                    var content = response.Content.ReadAsStreamAsync();
-                    var questArea = await JsonSerializer.DeserializeAsync<QuestArea>(await content);
-
-                    return Ok(questArea);
-                }
-                else
-                {
-                    throw new Exception("The response was status code " + response.StatusCode);
-                }
-            }
-            catch (Exception ex)
-            {
-                throw new Exception("Unhandled status code from 'GetQuestArea': " + ex.Message);
+                return Ok(questArea);
             }
 
+            return StatusCode((int)response.StatusCode);
         }
 
+        /// <summary>
+        /// Get a list of zones by name (case insensitive, partials accepted).
+        /// </summary>
+        /// <param name="name">The search term.</param>
+        /// <response code="200">Returns the resource.</response>
+        /// <response code="400">If the request is null or missing required fields.</response>
+        /// <response code="401">If the client failed to authorize an access token.</response>
+        /// <response code="404">If the resource was not found.</response>
         [HttpGet("search/{name}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
@@ -124,34 +86,25 @@ namespace WowDash.WebUI.Controllers
 
             request.Headers.TryAddWithoutValidation("Authorization", $"Bearer {access_token}");
 
-            try
+            var response = await client.SendAsync(request);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
+                return NotFound();
+
+            if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+                return Unauthorized();
+
+            if (response.IsSuccessStatusCode)
             {
-                var response = await client.SendAsync(request);
+                var content = response.Content.ReadAsStreamAsync();
+                var index = await JsonSerializer.DeserializeAsync<QuestAreaIndex>(await content);
 
-                if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
-                    return NotFound();
+                var results = index.Areas.Where(a => a.Name.Contains(name, StringComparison.OrdinalIgnoreCase)).ToList();
 
-                if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
-                    return Unauthorized();
-
-                if (response.IsSuccessStatusCode)
-                {
-                    var content = response.Content.ReadAsStreamAsync();
-                    var index = await JsonSerializer.DeserializeAsync<QuestAreaIndex>(await content);
-
-                    var results = index.Areas.Where(a => a.Name.Contains(name, StringComparison.OrdinalIgnoreCase)).ToList();
-
-                    return Ok(results);
-                }
-                else
-                {
-                    throw new Exception("The response was status code " + response.StatusCode);
-                }
+                return Ok(results);
             }
-            catch (Exception ex)
-            {
-                throw new Exception("Unhandled status code from 'SearchQuestAreasByName': " + ex.Message);
-            }
+
+            return StatusCode((int)response.StatusCode);
         }
     }
 }
