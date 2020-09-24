5b3ff03868cd4f4c52175c3ad690278d1ce22a95
jmia <jen.armstrong89@gmail.com>
2020-09-24 10:47:10 -0400

    Cleanup JournalInstances, add integration tests

---
+++ WowDash.IntegrationTests/Blizzard/JournalInstanceQueryTests.cs
@@ -0,0 +1,64 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using WowDash.ApplicationCore.Models;
+
+namespace WowDash.IntegrationTests.Blizzard
+{
+    [TestFixture]
+    public class JournalInstanceQueryTests : IntegrationTestBase
+    {
+        [Test]
+        public async Task GetJournalInstance_ReturnsJournalEncounter()
+        {
+            var expectedGameId = 751;
+            var expectedName = "Black Temple";
+
+            // Act
+            var httpResponse = await Client.GetAsync($"/api/dungeons/{expectedGameId}");
+
+            httpResponse.EnsureSuccessStatusCode();
+
+            var response = await httpResponse.Content.ReadAsStreamAsync();
+
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+            };
+
+            var result = await JsonSerializer.DeserializeAsync<JournalInstance>(response, options);
+
+            // Assert
+            result.Id.Should().Be(expectedGameId);
+            result.Name.Should().Be(expectedName);
+        }
+
+        [Test]
+        public async Task SearchJournalInstancesByName_ReturnsList()
+        {
+            var searchTerm = "karazhan";
+
+            // Act
+            var httpResponse = await Client.GetAsync($"/api/dungeons/search/{searchTerm}");
+
+            httpResponse.EnsureSuccessStatusCode();
+
+            var response = await httpResponse.Content.ReadAsStreamAsync();
+
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+            };
+
+            var result = await JsonSerializer.DeserializeAsync<IEnumerable<JournalInstance>>(response, options);
+
+            // Assert
+            result.Should().NotBeEmpty();
+            result.All(a => a.Name.Contains(searchTerm, StringComparison.OrdinalIgnoreCase)).Should().BeTrue();
+        }
+    }
+}
