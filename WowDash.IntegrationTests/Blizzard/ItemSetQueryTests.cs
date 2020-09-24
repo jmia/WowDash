b4e0500a4dac6f8d81ee4c55209c1118c8b6c5c8
Jen Armstrong <jen.armstrong89@gmail.com>
2020-09-24 10:05:46 -0400

    Add ItemSet queries and tests (#108)

---
+++ WowDash.IntegrationTests/Blizzard/ItemSetQueryTests.cs
@@ -0,0 +1,64 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using WowDash.ApplicationCore.Models;
+
+namespace WowDash.IntegrationTests.Blizzard
+{
+    [TestFixture]
+    public class ItemSetQueryTests : IntegrationTestBase
+    {
+        [Test]
+        public async Task GetItemset_ReturnsItemset()
+        {
+            var expectedGameId = 857;
+            var expectedName = "VanCleef's Battlegear";
+
+            // Act
+            var httpResponse = await Client.GetAsync($"/api/item-sets/{expectedGameId}");
+
+            httpResponse.EnsureSuccessStatusCode();
+
+            var response = await httpResponse.Content.ReadAsStreamAsync();
+
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+            };
+
+            var result = await JsonSerializer.DeserializeAsync<ItemSet>(response, options);
+
+            // Assert
+            result.Id.Should().Be(expectedGameId);
+            result.Name.Should().Be(expectedName);
+        }
+
+        [Test]
+        public async Task SearchItemSetsByName_ReturnsItemSetList()
+        {
+            var searchTerm = "thunder";
+
+            // Act
+            var httpResponse = await Client.GetAsync($"/api/item-sets/search/{searchTerm}");
+
+            httpResponse.EnsureSuccessStatusCode();
+
+            var response = await httpResponse.Content.ReadAsStreamAsync();
+
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+            };
+
+            var result = await JsonSerializer.DeserializeAsync<IEnumerable<ItemSet>>(response, options);
+
+            // Assert
+            result.Should().NotBeEmpty();
+            result.All(a => a.Name.Contains(searchTerm, StringComparison.OrdinalIgnoreCase)).Should().BeTrue();
+        }
+    }
+}
