7ff281f551314fe2d7d358bf37ef7fb08d47e0c7
jmia <jen.armstrong89@gmail.com>
2020-09-24 10:05:06 -0400

    Add ItemSet queries and tests

--- WowDash/WebUI/Controllers/ItemSetsController.cs
+++ WowDash/WebUI/Controllers/ItemSetsController.cs
@@ -18,52 +18,17 @@ namespace WowDash.WebUI.Controllers
         public ItemSetsController(IHttpClientFactory clientFactory, IConfiguration configuration)
             : base(clientFactory, configuration) { }
 
-        [HttpGet]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<ActionResult<IEnumerable<ItemSet>>> GetItemSets()
-        {
-            var client = _clientFactory.CreateClient();
-
-            var access_token = await GetAccessTokenAsync();
-
-            using var request = new HttpRequestMessage(new HttpMethod("GET"),
-                "https://us.api.blizzard.com/data/wow/item-set/index?" +
-                "namespace=static-us&locale=en_US");
-
-            request.Headers.TryAddWithoutValidation("Authorization", $"Bearer {access_token}");
-
-            try
-            {
-                var response = await client.SendAsync(request);
-
-                if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
-                    return NotFound();
-
-                if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
-                    return Unauthorized();
-
-                if (response.IsSuccessStatusCode)
-                {
-                    var content = response.Content.ReadAsStreamAsync();
-                    var index = await JsonSerializer.DeserializeAsync<ItemSetIndex>(await content);
-
-                    return Ok(index.ItemSets.ToList());
-                }
-                else
-                {
-                    throw new Exception("The response was status code " + response.StatusCode);
-                }
-            }
-            catch (Exception ex)
-            {
-                throw new Exception("Unhandled status code from 'GetItemSets': " + ex.Message);
-            }
-        }
-
+        /// <summary>
+        /// Get an item set by its game ID.
+        /// </summary>
+        /// <param name="id">The game ID of the item set.</param>
+        /// <response code="200">Returns the resource.</response>
+        /// <response code="400">If the request is null or missing required fields.</response>
+        /// <response code="401">If the client failed to authorize an access token.</response>
+        /// <response code="404">If the resource was not found.</response>
         [HttpGet("{id}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<ItemSet>> GetItemSet(int id)
@@ -106,8 +71,17 @@ namespace WowDash.WebUI.Controllers
             }
         }
 
+        /// <summary>
+        /// Search for an item set by its name (partials accepted, case insensitive).
+        /// </summary>
+        /// <param name="name">The search term.</param>
+        /// <response code="200">Returns a list of matched resources.</response>
+        /// <response code="400">If the request is null or missing required fields.</response>
+        /// <response code="401">If the client failed to authorize an access token.</response>
+        /// <response code="404">If the resource was not found.</response>
         [HttpGet("search/{name}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<IEnumerable<ItemSet>>> SearchItemSetsByName(string name)
