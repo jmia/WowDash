d03c4c3778e132917d7ff50f8da0a790ac069da4
jmia <jen.armstrong89@gmail.com>
2020-09-24 10:37:12 -0400

    Cleanup JournalEncounters, add integration tests

--- WowDash/WebUI/Controllers/JournalEncountersController.cs
+++ WowDash/WebUI/Controllers/JournalEncountersController.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Text.Json;
@@ -18,53 +17,17 @@ namespace WowDash.WebUI.Controllers
         public JournalEncountersController(IHttpClientFactory clientFactory, IConfiguration configuration) 
             : base(clientFactory, configuration) { }
 
-        [HttpGet]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-        [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<ActionResult<IEnumerable<JournalEncounter>>> GetJournalEncounters()
-        {
-            var client = _clientFactory.CreateClient();
-
-            var access_token = await GetAccessTokenAsync();
-
-            using var request = new HttpRequestMessage(new HttpMethod("GET"),
-                "https://us.api.blizzard.com/data/wow/journal-encounter/index?" +
-                "namespace=static-us&locale=en_US");
-
-            request.Headers.TryAddWithoutValidation("Authorization", $"Bearer {access_token}");
-
-            try
-            {
-                var response = await client.SendAsync(request);
-
-                if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
-                    return NotFound();
-
-                if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
-                    return Unauthorized();
-
-                if (response.IsSuccessStatusCode)
-                {
-                    var content = response.Content.ReadAsStreamAsync();
-                    var index = await JsonSerializer.DeserializeAsync<JournalEncounterIndex>(await content);
-
-                    return Ok(index.Encounters.ToList());
-                }
-                else
-                {
-                    throw new Exception("The response was status code " + response.StatusCode);
-                }
-            }
-            catch (Exception ex)
-            {
-                throw new Exception("Unhandled status code from 'GetJournalEncounters': " + ex.Message);
-            }
-
-        }
-
+        /// <summary>
+        /// Get a boss by its game ID.
+        /// </summary>
+        /// <param name="id">The game ID of the boss.</param>
+        /// <response code="200">Returns the resource.</response>
+        /// <response code="400">If the request is null or missing required fields.</response>
+        /// <response code="401">If the client failed to authorize an access token.</response>
+        /// <response code="404">If the resource was not found.</response>
         [HttpGet("{id}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<JournalEncounter>> GetJournalEncounter(int id)
@@ -79,36 +42,36 @@ namespace WowDash.WebUI.Controllers
 
             request.Headers.TryAddWithoutValidation("Authorization", $"Bearer {access_token}");
 
-            try
+            var response = await client.SendAsync(request);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
+                return NotFound();
+
+            if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+                return Unauthorized();
+
+            if (response.IsSuccessStatusCode)
             {
-                var response = await client.SendAsync(request);
+                var content = response.Content.ReadAsStreamAsync();
+                var journalEncounter = await JsonSerializer.DeserializeAsync<JournalEncounter>(await content);
 
-                if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
-                    return NotFound();
-
-                if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
-                    return Unauthorized();
-
-                if (response.IsSuccessStatusCode)
-                {
-                    var content = response.Content.ReadAsStreamAsync();
-                    var journalEncounter = await JsonSerializer.DeserializeAsync<JournalEncounter>(await content);
-
-                    return Ok(journalEncounter);
-                }
-                else
-                {
-                    throw new Exception("The response was status code " + response.StatusCode);
-                }
+                return Ok(journalEncounter);
             }
-            catch (Exception ex)
-            {
-                throw new Exception("Unhandled status code from 'GetJournalEncounter': " + ex.Message);
-            }
+
+            return StatusCode((int)response.StatusCode);
         }
 
+        /// <summary>
+        /// Get a boss by its name (case insensitive, partials accepted).
+        /// </summary>
+        /// <param name="name">The search term.</param>
+        /// <response code="200">Returns the resource.</response>
+        /// <response code="400">If the request is null or missing required fields.</response>
+        /// <response code="401">If the client failed to authorize an access token.</response>
+        /// <response code="404">If the resource was not found.</response>
         [HttpGet("search/{name}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<IEnumerable<JournalEncounter>>> SearchJournalEncountersByBossName(string name)
@@ -124,37 +87,37 @@ namespace WowDash.WebUI.Controllers
 
             request.Headers.TryAddWithoutValidation("Authorization", $"Bearer {access_token}");
 
-            try
+            var response = await client.SendAsync(request);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
+                return NotFound();
+
+            if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+                return Unauthorized();
+
+            if (response.IsSuccessStatusCode)
             {
-                var response = await client.SendAsync(request);
+                var content = response.Content.ReadAsStreamAsync();
+                var searchResult = await JsonSerializer.DeserializeAsync<BlizzardSearchResult>(await content);
 
-                if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
-                    return NotFound();
-
-                if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
-                    return Unauthorized();
-
-                if (response.IsSuccessStatusCode)
-                {
-                    var content = response.Content.ReadAsStreamAsync();
-                    var searchResult = await JsonSerializer.DeserializeAsync<BlizzardSearchResult>(await content);
-
-                    return Ok(searchResult.Results.Select(r => new JournalEncounter { Id = r.Data.Id, Name = r.Data.Name.en_US })
-                                               .ToList());
-                }
-                else
-                {
-                    throw new Exception("The response was status code " + response.StatusCode);
-                }
+                return Ok(searchResult.Results.Select(r => new JournalEncounter { Id = r.Data.Id, Name = r.Data.Name.en_US })
+                                            .ToList());
             }
-            catch (Exception ex)
-            {
-                throw new Exception("Unhandled status code from 'SearchJournalEncountersByBossName': " + ex.Message);
-            }
+
+            return StatusCode((int)response.StatusCode);
         }
 
+        /// <summary>
+        /// Get a list of bosses by their parent dungeon (case insensitive, partials accepted).
+        /// </summary>
+        /// <param name="name">The search term.</param>
+        /// <response code="200">Returns the resource.</response>
+        /// <response code="400">If the request is null or missing required fields.</response>
+        /// <response code="401">If the client failed to authorize an access token.</response>
+        /// <response code="404">If the resource was not found.</response>
         [HttpGet("search/dungeon/{name}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<IEnumerable<JournalEncounter>>> SearchJournalEncountersByInstanceName(string name)
@@ -170,33 +133,24 @@ namespace WowDash.WebUI.Controllers
 
             request.Headers.TryAddWithoutValidation("Authorization", $"Bearer {access_token}");
 
-            try
+            var response = await client.SendAsync(request);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
+                return NotFound();
+
+            if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+                return Unauthorized();
+
+            if (response.IsSuccessStatusCode)
             {
-                var response = await client.SendAsync(request);
+                var content = response.Content.ReadAsStreamAsync();
+                var searchResult = await JsonSerializer.DeserializeAsync<BlizzardSearchResult>(await content);
 
-                if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
-                    return NotFound();
-
-                if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
-                    return Unauthorized();
-
-                if (response.IsSuccessStatusCode)
-                {
-                    var content = response.Content.ReadAsStreamAsync();
-                    var searchResult = await JsonSerializer.DeserializeAsync<BlizzardSearchResult>(await content);
-
-                    return Ok(searchResult.Results.Select(r => new JournalEncounter { Id = r.Data.Id, Name = r.Data.Name.en_US })
-                                               .ToList());
-                }
-                else
-                {
-                    throw new Exception("The response was status code " + response.StatusCode);
-                }
+                return Ok(searchResult.Results.Select(r => new JournalEncounter { Id = r.Data.Id, Name = r.Data.Name.en_US })
+                                            .ToList());
             }
-            catch (Exception ex)
-            {
-                throw new Exception("Unhandled status code from 'SearchJournalEncountersByInstanceName': " + ex.Message);
-            }
+
+            return StatusCode((int)response.StatusCode);
         }
     }
 }
