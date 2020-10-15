2d3bbfa5b523a6f3adaa51ac0354615a5eb68be0
jmia <jen.armstrong89@gmail.com>
2020-10-15 19:35:31 -0400

    add footer and fix styling

--- WowDash/WebUI/ClientApp/src/App.vue
+++ WowDash/WebUI/ClientApp/src/App.vue
@@ -1,66 +1,74 @@
 <template>
   <div class="bg-black-alt font-sans leading-normal tracking-normal">
     <AppBar />
-      <div class="container w-full mx-auto pt-20">
-        <div class="w-full px-4 md:px-0 md:mt-8 mb-16 text-gray-800 leading-normal">
-          <router-view></router-view>
-        </div>
+    <div class="container min-h-screen w-full mx-auto pt-20">
+      <div
+        class="w-full px-4 md:px-0 md:mt-8 mb-16 text-gray-800 leading-normal"
+      >
+        <router-view></router-view>
       </div>
-    <!-- a footer might go here -->
+    </div>
+    <AppFooter />
   </div>
 </template>
 
 <script>
-import AppBar from './components/AppBar'
+import AppBar from "./components/AppBar";
+import AppFooter from "./components/AppFooter";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    AppBar
+    AppBar,
+    AppFooter,
   },
   mounted() {
     // Add Google Sign-In JavaScript CDN
-    let googleScript = document.createElement('script')
-    googleScript.setAttribute('src', 'https://apis.google.com/js/api:client.js')
-    document.head.appendChild(googleScript)
+    let googleScript = document.createElement("script");
+    googleScript.setAttribute(
+      "src",
+      "https://apis.google.com/js/api:client.js"
+    );
+    document.head.appendChild(googleScript);
 
     // Add Wowhead Tooltips JavaScript
-    let wowheadScript = document.createElement('script')
-    wowheadScript.setAttribute('src', 'https://wow.zamimg.com/widgets/power.js')
-    document.head.appendChild(wowheadScript)
+    let wowheadScript = document.createElement("script");
+    wowheadScript.setAttribute(
+      "src",
+      "https://wow.zamimg.com/widgets/power.js"
+    );
+    document.head.appendChild(wowheadScript);
   },
-  data () {
-    return {
-    }
+  data() {
+    return {};
   },
-  methods: {
-  }
-}
+  methods: {},
+};
 </script>
 
 <style>
-  .bg-black-alt  {
-    background:#191919;
-  }
-  .table-title {
-    @apply font-bold;
-    @apply uppercase;
-    @apply text-blue-400;
-  }
-  .dark-rounded {
-    @apply bg-gray-900;
-    @apply border;
-    @apply border-gray-800;
-    @apply rounded;
-    @apply shadow;
-  }
-  .checkbox-text {
-    @apply text-left;
-    @apply pl-2; 
-    @apply text-gray-400;
-  }
-  .checkbox-size {
-    @apply w-4;
-    @apply h-4;
-  }
+.bg-black-alt {
+  background: #191919;
+}
+.table-title {
+  @apply font-bold;
+  @apply uppercase;
+  @apply text-blue-400;
+}
+.dark-rounded {
+  @apply bg-gray-900;
+  @apply border;
+  @apply border-gray-800;
+  @apply rounded;
+  @apply shadow;
+}
+.checkbox-text {
+  @apply text-left;
+  @apply pl-2;
+  @apply text-gray-400;
+}
+.checkbox-size {
+  @apply w-4;
+  @apply h-4;
+}
 </style>
