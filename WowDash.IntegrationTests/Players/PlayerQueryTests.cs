0708d91f8c7abd1b9cf3e7316f05c68fc8395de4
jmia <jen.armstrong89@gmail.com>
2020-10-30 17:10:58 -0400

    Add player profile endpoints and tests

---
+++ WowDash.IntegrationTests/Players/PlayerQueryTests.cs
@@ -0,0 +1,50 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System.Text.Json;
+using WowDash.ApplicationCore.DTO.Responses;
+using WowDash.ApplicationCore.Entities;
+using static WowDash.ApplicationCore.Common.Enums;
+
+namespace WowDash.IntegrationTests.Players
+{
+    public class PlayerQueryTests : IntegrationTestBase
+    {
+        [Test]
+        public async System.Threading.Tasks.Task GetPlayerProfile_ReturnsPlayerProfile()
+        {
+            // Arrange
+            var expectedDefaultTaskType = TaskType.Collectible;
+            var expectedDisplayName = "Thrall";
+            var expectedDefaultRealm = "thrall";
+
+            var player = new Player()
+            {
+                DisplayName = expectedDisplayName,
+                DefaultRealm = expectedDefaultRealm,
+                DefaultTaskType = expectedDefaultTaskType
+            };
+
+            var addedPlayer = await AddAsync(player);
+
+            // Act
+            var httpResponse = await Client.GetAsync($"/api/players/{addedPlayer.Id}");
+
+            httpResponse.EnsureSuccessStatusCode();
+
+            var response = await httpResponse.Content.ReadAsStreamAsync();
+
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+            };
+
+            var result = await JsonSerializer.DeserializeAsync<GetPlayerProfileResponse>(response, options);
+
+            // Assert
+            result.PlayerId.Should().Be(player.Id);
+            result.DisplayName.Should().Be(expectedDisplayName);
+            result.DefaultTaskType.Should().Be(expectedDefaultTaskType);
+            result.DefaultRealm.Should().Be(expectedDefaultRealm);
+        }
+    }
+}
---
+++ WowDash/ApplicationCore/DTO/Requests/SetPlayerProfileRequest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using static WowDash.ApplicationCore.Common.Enums;
+
+namespace WowDash.ApplicationCore.DTO.Requests
+{
+    public class SetPlayerProfileRequest
+    {
+        public Guid PlayerId { get; set; }
+        public string DisplayName { get; set; }
+        public TaskType? DefaultTaskType { get; set; }
+        public string DefaultRealm { get; set; }
+
+        public SetPlayerProfileRequest() { }
+
+        public SetPlayerProfileRequest(Guid playerId, string displayName, TaskType? defaultTaskType, string defaultRealm)
+        {
+            PlayerId = playerId;
+            DisplayName = displayName;
+            DefaultTaskType = defaultTaskType;
+            DefaultRealm = defaultRealm;
+        }
+    }
+}
